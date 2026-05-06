2bfc4052210b6c91d5422566096737bdaa2177ea
Astha Thapliyal <aasthathapliyal5@gmail.com>
2026-05-06 22:34:11 +0530

    Added Validation to the RegisterRequest and LoginRequest

--- auth-service/src/main/java/com/dhvani/auth/dto/LoginRequest.java
+++ auth-service/src/main/java/com/dhvani/auth/dto/LoginRequest.java
@@ -1,8 +1,14 @@
 package com.dhvani.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
 
+    @Email
+    @NotBlank
     private String email;
+    @NotBlank
     private String password;
 
     public String getEmail() {
--- auth-service/src/main/java/com/dhvani/auth/dto/RegisterRequest.java
+++ auth-service/src/main/java/com/dhvani/auth/dto/RegisterRequest.java
@@ -1,9 +1,20 @@
 package com.dhvani.auth.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class RegisterRequest {
 
+    @Email
+    @NotBlank
     private String email;
+
+    @NotBlank
+    @Size(min = 6)
     private String password;
+
+    @NotBlank
     private String role;
 
     public String getEmail() {return email;}
