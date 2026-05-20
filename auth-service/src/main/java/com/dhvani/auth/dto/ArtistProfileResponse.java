6c88b5a20d6f5c3f754c89ccbd456b840c249e7b
Astha Thapliyal <aasthathapliyal5@gmail.com>
2026-05-20 22:17:09 +0530

    refactor : made some minor changes in ArtistProfileResponse

--- auth-service/src/main/java/com/dhvani/auth/dto/ArtistProfileResponse.java
+++ auth-service/src/main/java/com/dhvani/auth/dto/ArtistProfileResponse.java
@@ -33,27 +33,55 @@ public class ArtistProfileResponse {
         return id;
     }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public String getArtistName() {
         return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
     }
 
     public String getGenre() {
         return genre;
     }
 
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
     public String getBio() {
         return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
     }
 
     public Integer getBpm() {
         return bpm;
     }
 
+    public void setBpm(Integer bpm) {
+        this.bpm = bpm;
+    }
+
     public String getRaag() {
         return raag;
     }
 
+    public void setRaag(String raag) {
+        this.raag = raag;
+    }
+
     public String getMusicalKey() {
         return musicalKey;
+    }
+
+    public void setMusicalKey(String musicalKey) {
+        this.musicalKey = musicalKey;
     }
 }
